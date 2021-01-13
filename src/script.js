1dc41a0115317829355e20c42c4cdee09eea7dcd
SlaVistaPL <53293133+SlaVistaPL@users.noreply.github.com>
2021-01-13 15:15:03 +0100

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 /*
-	MidaCSS Framework v1.0
+	MidaCSS Framework v1.3
 	(c) 2020, SlaVistaPL (https://ampersandbot.pl)
 */
 
@@ -11,28 +11,42 @@ window.MidaCSS = class MidaCSS {
     static addFramework(name) {
         switch(name) {
             case "FontAwesome":
-                console.log("FontAwesome has been added to MidaCSS and is now ready to use.");
+                console.log("FontAwesome został dodany do MidaCSS oraz jest gotowy do użycia.");
 				var fontAwesome = document.createElement("script");
 				fontAwesome.src = "https://kit.fontawesome.com/604acb9c5e.js";
 				document.head.appendChild(fontAwesome);
                 break;
 			case "Underscore.js":
-				console.log("Underscore.js has been added to MidaCSS and is now ready to use.");
+				console.log("Underscore.js został dodany do MidaCSS oraz jest gotowy do użycia.");
 				var underscore = document.createElement("script");
 				underscore.src = "https://cdn.jsdelivr.net/npm/underscore@1.11.0/underscore-min.js";
 				document.head.appendChild(underscore);
                 break;
 			case "Lodash":
-				console.log("Lodash has been added to MidaCSS and is now ready to use.")
+				console.log("Lodash został dodany do MidaCSS oraz jest gotowy do użycia.")
 				var lodash = document.createElement("script");
 				lodash.src = "https://cdn.jsdelivr.net/npm/lodash@4.17.20/lodash.min.js";
 				document.head.appendChild(lodash);
 				break;
 			case "jQuery":
-				console.log("jQuery has been added to MidaCSS and is now ready to use.")
+				console.log("jQuery został dodany do MidaCSS oraz jest gotowy do użycia.")
 				var jquery = document.createElement("script");
 				jquery.src = "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js";
 				document.head.appendChild(jquery);
+				break;
+			case "React":
+				console.log("React został dodany do MidaCSS oraz jest gotowy do użycia.")
+				var react = document.createElement("script");
+				var react2 = document.createElement("script");
+				react.src = "https://unpkg.com/react@17/umd/react.production.min.js";
+				react2.src = "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js";
+				document.head.appendChild(react);
+				break;
+			case "Vue":
+				console.log("Vue został dodany do MidaCSS oraz jest gotowy do użycia.")
+				var vue = document.createElement("script");
+				vue.src = "https://cdn.jsdelivr.net/npm/vue@2.6.12";
+				document.head.appendChild(vue);
 				break;
             default:
                 console.error(`${name} does not exist in MidaCSS framework library. Check if you have typed package name properly.`);
